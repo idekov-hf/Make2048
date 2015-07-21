ef749476a57f101e51d954d29ac690fe92504d87
Iavor Dekov <ivdekov@gmail.com>
2015-07-21 09:58:13 -0400

    Started the Make2048 game tutorial. Finished initial game setup in SpriteBuilder, made game classes (Tile.swift and Grid.swift) in Xcode.

---
+++ Make2048.spritebuilder/Grid.swift
@@ -0,0 +1,13 @@
+//
+//  Grid.swift
+//  Make2048
+//
+//  Created by Iavor Dekov on 7/21/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation
+
+class Grid: CCNodeColor {
+    
+}
---
+++ Make2048.spritebuilder/Tile.swift
@@ -0,0 +1,13 @@
+//
+//  Tile.swift
+//  Make2048
+//
+//  Created by Iavor Dekov on 7/21/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation
+
+class Tile: CCNode {
+    
+}
